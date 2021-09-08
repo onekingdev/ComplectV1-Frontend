51e1dab83f20d267b901ba0f337be4aa5a285379
Constantine Gololobov <dev@gololobov.dev>
2021-09-08 19:41:38 +0200

    Start kit

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM node:14.17.6-alpine
 RUN apk update && apk upgrade
 
 WORKDIR /home/app
+RUN npm i -g @vue/cli
 COPY package.json ./
 RUN npm i
---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	configureWebpack: {
+		devServer: {
+			public: "localhost:81",
+			host: "0.0.0.0",
+			port: 5001,
+			// https: true,
+			// hmr: true,
+			disableHostCheck: true,
+			headers: {
+				"Access-Control-Allow-Origin": "*"
+			}
+		}
+	}
+}
