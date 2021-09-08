0a774fd26348b191211f81982739a3b43dc77ac4
Constantine Gololobov <dev@gololobov.dev>
2021-09-08 19:57:45 +0200

    Babel Config

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ## Run
 1. Start dev server: `docker-compose up`
 2. Navigate to `localhost:81`
+
+
+## To-Do
+1. Eslint
+2. Babel
+	* transform-class-properties ???
--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,19 @@
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
+	"presets": [
+		["@babel/preset-env", {
+			"modules": false,
+			"targets": {
+				"browsers": "> 1%",
+				"uglify": true
+			},
+			"useBuiltIns": false
+		}]
+	],
+
+	"plugins": [
+		"@babel/syntax-dynamic-import",
+		"@babel/plugin-proposal-object-rest-spread",
+		// ["transform-class-properties", { "spec": true }],
+		["@babel/plugin-transform-runtime", { "regenerator": true }]
+	]
 }
