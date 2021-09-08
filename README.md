bfd0fe7ecbf7fa475234787dc04b043c6e296682
Constantine Gololobov <dev@gololobov.dev>
2021-09-08 22:01:29 +0200

    Ports update

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Complect - Frontend
 
-## Run
+## Run in Docker Container
 1. Start dev server: `docker-compose up`
+2. Navigate to `localhost:81`
+
+## or Run locally
+1. Start dev server: `npm run dev`
 2. Navigate to `localhost:81`
 
 
--- vue.config.js
+++ vue.config.js
@@ -9,13 +9,13 @@ module.exports = {
 		devServer: {
 			public: "localhost:81",
 			host: "0.0.0.0",
-			port: 5001,
+			port: 81,
 			// https: true,
 			// hmr: true,
 			disableHostCheck: true,
 			headers: {
 				"Access-Control-Allow-Origin": "*",
-			},
-		},
-	},
+			}
+		}
+	}
 }
