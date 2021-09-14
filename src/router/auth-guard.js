4cb297e3656341757c79cc5b92d6c4fc3ae2aefd
Constantine Gololobov <dev@gololobov.dev>
2021-09-14 22:13:47 +0200

    Update auth-guard.js

--- src/router/auth-guard.js
+++ src/router/auth-guard.js
@@ -1,5 +1,11 @@
 import store from '@/store/globalStore'
 
+// Need to know how get current user type / bad unstable temp solution
+const userType = localStorage.getItem('app.currentUser.userType') ? JSON.parse(localStorage.getItem('app.currentUser.userType')) : ''
+
+const plans = ['free', 'business', 'team']
+const roles = ['basic', 'trusted', 'admin']
+
 const AuthGuard = (to, from, next) => {
   if(!store.getters['roles/currentPlan']) next({ name: 'sign-in'})
   else if (store.getters['roles/currentPlan'] === 'free') next(`/access-denied`)
@@ -7,17 +13,12 @@ const AuthGuard = (to, from, next) => {
 }
 
 const BusinessGuard = (to, from, next) => {
-  if (store.getters['roles/currentPlan'] !== 'business') next(`/access-denied`)
+  if (userType !== 'business') next(`/access-denied`)
   else next()
 }
 
 const SpecialistGuard = (to, from, next) => {
-  // console.log(store.getters['roles/roles'])
-  // console.log(store.getters['roles/currentRole'])
-  // console.log(store.getters['roles/businessID'])
-  // console.log(store.getters['roles/currentAccount'])
-  console.log(store.getters['roles/currentRole'])
-  if (store.getters['roles/currentPlan'] !== 'specialist_pro') next(`/access-denied`)
+  if (userType !== 'specialists') next(`/access-denied`)
   else next()
 }
 
