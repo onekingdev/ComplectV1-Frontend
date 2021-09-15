680a311640b1e97d50a24e4d8e80418a201b6287
Tikhon Sokolov <tikhon.sokolov>
2021-09-16 00:45:23 +0300

    Add headers to ModalLoader

--- src/common/rest/ModelLoader.vue
+++ src/common/rest/ModelLoader.vue
@@ -37,7 +37,7 @@ export default {
       if (this.url) {
         fetch(this.$store.getters.backendUrl+this.url, {
           method: 'GET',
-          headers: {'Accept': 'application/json', 'Content-Type': 'application/json'}
+          ...this.$store.getters.authHeaders
         }).then(response => response.json())
           .then(result => {
             this.$emit('loaded', onlyKnownProperties(this.callback(result), this.default()))
