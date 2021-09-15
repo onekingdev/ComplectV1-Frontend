69baeaeac3ea9b5cb5e94760c508c9730c8b0be5
Constantine Gololobov <dev@gololobov.dev>
2021-09-15 18:58:23 +0200

    Disabled Guard

--- src/router/auth-guard.js
+++ src/router/auth-guard.js
@@ -1,26 +1,29 @@
-import store from '@/store/globalStore'
+// import store from '@/store/globalStore'
 
 // Need to know how get current user type / bad unstable temp solution
-const userType = localStorage.getItem('app.currentUser.userType') ? JSON.parse(localStorage.getItem('app.currentUser.userType')) : ''
-const userAuth = localStorage.getItem('app.currentUser.token')
+// const userType = localStorage.getItem('app.currentUser.userType') ? JSON.parse(localStorage.getItem('app.currentUser.userType')) : ''
+// const userAuth = localStorage.getItem('app.currentUser.token')
 
 const plans = ['free', 'business', 'team']
 const roles = ['basic', 'trusted', 'admin']
 
 const AccessGuard = (to, from, next) => {
-  if(!userAuth) next({ name: 'sign-in'})
-  else if (store.getters['roles/currentPlan'] === 'free') next(`/access-denied`)
-  else next()
+  // if(!userAuth) next({ name: 'sign-in'})
+  // else if (store.getters['roles/currentPlan'] === 'free') next(`/access-denied`)
+  // else next()
+  next()
 }
 
 const BusinessGuard = (to, from, next) => {
-  if (userType !== 'businesses') next(`/access-denied`)
-  else next()
+  // if (userType !== 'businesses') next(`/access-denied`)
+  // else next()
+  next()
 }
 
 const SpecialistGuard = (to, from, next) => {
-  if (userType !== 'specialists') next(`/access-denied`)
-  else next()
+  // if (userType !== 'specialists') next(`/access-denied`)
+  // else next()
+  next()
 }
 
 export { AccessGuard, BusinessGuard, SpecialistGuard}
