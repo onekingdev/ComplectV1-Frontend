c3f6b2bc1c37616123e1b1fe5fa52b92e6115627
Constantine Gololobov <dev@gololobov.dev>
2021-09-15 10:28:01 +0200

    Temp Routes Guard

--- src/router/auth-guard.js
+++ src/router/auth-guard.js
@@ -2,12 +2,13 @@ import store from '@/store/globalStore'
 
 // Need to know how get current user type / bad unstable temp solution
 const userType = localStorage.getItem('app.currentUser.userType') ? JSON.parse(localStorage.getItem('app.currentUser.userType')) : ''
+const userAuth = localStorage.getItem('app.currentUser.token')
 
 const plans = ['free', 'business', 'team']
 const roles = ['basic', 'trusted', 'admin']
 
 const AccessGuard = (to, from, next) => {
-  if(!store.getters['roles/currentPlan']) next({ name: 'sign-in'})
+  if(!userAuth) next({ name: 'sign-in'})
   else if (store.getters['roles/currentPlan'] === 'free') next(`/access-denied`)
   else next()
 }
