b49c97f6b60bfbdac2229820557a1af224b39022
Constantine Gololobov <dev@gololobov.dev>
2021-09-16 13:01:47 +0200

    appModule

--- src/router/auth-guard.js
+++ src/router/auth-guard.js
@@ -1,31 +1,22 @@
 import store from '@/store/globalStore'
 import loadLocalStorageToken from './loadLocalStorageToken'
+const appModule = store.getters.appModule
 
-// Need to know how get current user type / bad unstable temp solution
-// const userType = localStorage.getItem('app.currentUser.userType') ? JSON.parse(localStorage.getItem('app.currentUser.userType')) : ''
-// const userAuth = localStorage.getItem('app.currentUser.token')
-
-const plans = ['free', 'business', 'team']
-const roles = ['basic', 'trusted', 'admin']
 
 const AccessGuard = (to, from, next) => {
   loadLocalStorageToken(store)
-
   const userAuth = !!store.state.auth.loggedIn
   if (!userAuth) next({ name: 'sign-in'})
   else next()
-  // else if (store.getters['roles/currentPlan'] === 'free') next(`/access-denied`)
 }
 
 const BusinessGuard = (to, from, next) => {
-  const userType = store.getters.userType
-  if (`${userType}`.indexOf('business') !== 0) next(`/access-denied`)
+  if (appModule !== 'business') next(`/access-denied`)
   else next()
 }
 
 const SpecialistGuard = (to, from, next) => {
-  const userType = store.getters.userType
-  if (`${userType}`.indexOf('specialist') !== 0) next(`/access-denied`)
+  if (appModule !== 'specialist') next(`/access-denied`)
   else next()
 }
 
