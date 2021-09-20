efb3e5ed8b2a7c3abc2d546ca3a9429f863174f2
Tikhon Sokolov <tikhon.sokolov>
2021-09-21 01:27:05 +0300

    Fix indent, lint

--- src/common/Dashboard/components/Download/index.vue
+++ src/common/Dashboard/components/Download/index.vue
@@ -1,17 +1,9 @@
 <template lang="pug">
   div
-    //b-dropdown.mr-2(variant="default")
-    //  template(#button-content)
-    //    | Monhly
-    //    b-icon.ml-2(icon="chevron-down")
-    //  b-dropdown-item Annually
-    //  b-dropdown-item Monhly
-    //  b-dropdown-item Weekly
-    //  b-dropdown-item Daily
     b-dropdown#dropdown-form.m-0(ref='dropdown' variant="default" @hide='handleHide($event)' @hidden='isCloseable=false' right)
       template(#button-content)
         b Download
-        //b-icon.ml-2(icon="chevron-down")
+        //- b-icon.ml-2(icon="chevron-down")
       b-dropdown-form(style="width: 400px;" right)
         b-row.m-t-1(no-gutters)
           .col-sm.m-r-1
@@ -41,69 +33,65 @@ const forceDownload = (response, filename) => {
 const downloadBinary = (url, filename) => axios.get(`..${url}`, { responseType: 'blob' })
   .then(response => forceDownload(response, filename))
 
-  export default {
-    props: {
-      pdfUrl: {
-        type: String,
-        required: true
-      },
-      right: Boolean
+export default {
+  props: {
+    pdfUrl: {
+      type: String,
+      required: true
     },
-    data() {
-      return {
-        download: {
-          start_date: '',
-          end_date: '',
-        },
-        errors: {},
-        isCloseable: false,
-      }
-    },
-    methods: {
-      handleHide(bvEvent) {
-        // this.isCloseable = !this.isCloseable;
-        if (!this.isCloseable) {
-          bvEvent.preventDefault();
-        } else {
-          this.$refs.dropdown.hide();
-        }
+    right: Boolean
+  },
+  data() {
+    return {
+      download: {
+        start_date: '',
+        end_date: '',
       },
-      downloadBinary(url, filename) {
-        this.closeMe()
-        downloadBinary(url, filename)
-      },
-      closeMe() {
-        this.isCloseable = true;
-        this.$refs.dropdown.hide();
-      },
-      close (e) {
-        if (!this.$el.contains(e.target)) {
-          this.isCloseable = false
-        }
-      }
-    },
-    computed: {
-      pdfRangeDownload () {
-        const start_date = this.download.start_date
-        const end_date = this.download.end_date
-        const url = `/reminders.csv?from_date=${start_date}&to_date=${end_date}`
-        return url
-      },
-      datepickerOptions() {
-        return {
-          min: new Date().toISOString()
-        }
-      },
-    },
-    mounted () {
-      document.addEventListener('click', this.close)
-    },
-    beforeDestroy () {
-      document.removeEventListener('click',this.close)
+      errors: {},
+      isCloseable: false,
     }
+  },
+  methods: {
+    handleHide(bvEvent) {
+      // this.isCloseable = !this.isCloseable;
+      if (!this.isCloseable) {
+        bvEvent.preventDefault();
+      } else {
+        this.$refs.dropdown.hide();
+      }
+    },
+    downloadBinary(url, filename) {
+      this.closeMe()
+      downloadBinary(url, filename)
+    },
+    closeMe() {
+      this.isCloseable = true;
+      this.$refs.dropdown.hide();
+    },
+    close (e) {
+      if (!this.$el.contains(e.target)) {
+        this.isCloseable = false
+      }
+    }
+  },
+  computed: {
+    pdfRangeDownload () {
+      const start_date = this.download.start_date
+      const end_date = this.download.end_date
+      const url = `/reminders.csv?from_date=${start_date}&to_date=${end_date}`
+      return url
+    },
+    datepickerOptions() {
+      return {
+        min: new Date().toISOString()
+      }
+    },
+  },
+  mounted () {
+    document.addEventListener('click', this.close)
+  },
+  beforeDestroy () {
+    document.removeEventListener('click',this.close)
   }
+}
 </script>
-
-<style scoped>
-
-</style>
