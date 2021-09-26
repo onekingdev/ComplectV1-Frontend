93c5cabc118cd53b9818ae913bcbf177fb601a4b
Constantine Gololobov <dev@gololobov.dev>
2021-09-26 19:59:19 +0200

    Base Proposal Modal

--- src/business/projects/ProposalModal.vue
+++ src/business/projects/ProposalModal.vue
@@ -1,76 +1,31 @@
 <template lang="pug">
-  b-modal.fade(:id="modalId" title="Messages" size="xl" no-stacking)
-    .row
-      .col-lg-6.pr-lg-2
-        .card.messages-info
-          .card-body.p-0
-            .messages-info__internal.d-flex.align-items-center
-              UserAvatar(:user="application.specialist" :bg="true")
-              .d-block.m-l-2
-                h5.messages-info__title {{ application.specialist.first_name }} {{ application.specialist.last_name }}
-                .messages-info__state {{ application.specialist.location }}
-                StarsRating(:rate="Math.floor(Math.random() * 5)")
-              b-icon.linkedin.ml-auto.mt-auto(icon='linkedin' font-scale="1.5")
-            .messages-info__list(v-if="application.pricing_type === 'fixed'")
-              .messages-info__item
-                .messages-info__item-title Bid Price
-                .messages-info__item-text {{ application.fixed_budget | usdWhole }}
-              .messages-info__item
-                .messages-info__item-title Hourly
-                .messages-info__item-text {{ application.hourly_rate | usdWhole }}
-              .messages-info__item
-                .messages-info__item-title Payment Schedule
-                .messages-info__item-text {{ paymentScheduleReadable(application) }}
-            .messages-info__internal
-              dl.row.mb-0
-                //dt.col-sm-3.label About Me
-                //dd.col-sm-9
-                //  | Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-                dt.col-sm-3.label Start Date
-                dd.col-sm-9 {{ application.starts_on | asDate }}
-                dt.col-sm-3.label Due Date
-                dd.col-sm-9 {{ application.ends_on | asDate }}
-
-                dt.col-sm-3.label Role Details
-                dd.col-sm-9 {{ application.role_details }}
-                dt.col-sm-3.label Key Deliverables
-                dd.col-sm-9 {{ application.key_deliverables }}
-                dt.col-sm-3.label Attachment
-                dd.col-sm-9(v-if="application.document")
-                  a(:href="attachmentUrl(application.document)" target="_blank") {{ application.document.metadata.filename }}
-                dd.col-sm-9(v-else) -
-
-      .col-lg-6.pl-lg-2
-        .card-body.white-card-body.messages-border.p-0
-          .row
-            .col.p-y-1.mx-3
-              h5.mb-0 Messages
-          hr.my-0
-          .row
-            .col
-              .card-body.px-3.py-1
-                Messages(:messages="messages")
-          hr
-          b-row
-            .col
-              .card-body.p-3.position-relative
-                label.form-label Comment
-                Tiptap(v-model="message.comment" placeholder="Make a comment or leave a note...")
-                button.btn.btn-dark.save-comment-btn Send
-
-    template(#modal-footer="{ ok, cancel, hide }")
-      button.btn.btn-link(@click="hide") Cancel
-      button.btn.btn-outline-dark(v-if="!hasSpecialist(application.project)" v-b-modal="'DenyProposalConfirm'") Reject
-      button.btn.btn-dark(v-if="!hasSpecialist(application.project)" v-b-modal="confirmModalId") Accept
+  b-modal.fade(:id="modalId" :title="`Messages with ${application.specialist.first_name}`" size="xl" no-stacking)
+    .proposal-box
+      .info-box
+        .header
+          UserAvatar(:user="application.specialist" :bg="true")
+          .name {{ application.specialist.first_name }} {{ application.specialist.last_name }}
+          .location {{ application.specialist.location }}
+          StarsRating(:rate="application.specialist.ratings_average")
+        .content
+          .section(v-for="(content, title) in specialistInformation")
+            .title {{ title }}
+            .content {{ content }}
+      .messages-box
+        Get(:messages="`/api/reminders/${taskId}/messages`" :etag="etagMessages"): template(v-slot="{ messages }"): .card-body.p-0
+          Messages(:messages="messages" ref="Messages" @created="scrollMessages" @saved="newEtagMessages")
+        .input-area
+          label.form-label Comment
+          textarea-autosize.w-100.form-control.d-block(v-model="message.message" :min-height="100")
+          Errors(:errors="errors.message")
+          Post(:action="`/api/reminders/${taskId}/messages`" :model="{ message }" @errors="messageErrors = $event" @saved="messageSaved" alignRight)
+            button.btn.btn-primary.save-comment-btn Send
 </template>
 
 <script>
 import SpecialistDetails from './SpecialistDetails'
-import { redirectWithToast } from '@/common/Toast'
+import StarsRating from "@/business/marketplace/components/StarsRating"
 import { FIXED_PAYMENT_SCHEDULE_OPTIONS } from '@/common/ProjectInputOptions'
-
-import Tiptap from '@/common/Tiptap'
-import StarsRating from "@/business/marketplace/components/StarsRating";
 import Messages from '@/common/Messages'
 
 export default {
@@ -106,13 +61,65 @@ export default {
   computed: {
     hasSpecialist: () => project => !!project.specialist_id,
     paymentScheduleReadable: () => application => FIXED_PAYMENT_SCHEDULE_OPTIONS[application.payment_schedule],
-    attachmentUrl: () => document => `/uploads/${document.storage}/${document.id}`
+    attachmentUrl: () => document => `/uploads/${document.storage}/${document.id}`,
+    specialistInformation() {
+      return {
+        "Rate": '$'+this.application.specialist.min_hourly_rate,
+        "Industries": this.application.specialist.industries.map(industry => industry.name).join(', '),
+        "Jurisdictions": this.application.specialist.jurisdictions.map(jurisdiction => jurisdiction.name).join(', '),
+        "About Me": this.application.specialist.description,
+      }
+    }
   },
   components: {
     SpecialistDetails,
-    Tiptap,
     StarsRating,
     Messages,
   }
 }
 </script>
+
+<style lang="scss">
+.modal-title {
+  text-transform: none
+}
+</style>
+
+<style lang="scss" scoped>
+.proposal-box {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  .info-box, .messages-box {
+    // padding: 1.25rem;
+    border: solid 1px #DCDEE4;
+    border-radius: 4px;
+  }
+  .info-box {
+    flex: 1 1 350px;
+    font-size: 0.875rem;
+    .header {
+      padding: 2rem 1.25rem;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+    .content {
+      .section {
+        padding: 1.25rem;
+        border-top: solid 1px #DCDEE4;
+        .title {
+          font-weight: bold;
+        }
+      }
+    }
+  }
+  .messages-box {
+    flex: 1 0 auto;
+    .input-area {
+      border-top: solid 1px #DCDEE4;
+      padding: 1.25rem;
+    }
+  }
+}
+</style>
