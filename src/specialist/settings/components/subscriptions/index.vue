d4246efbe4467b6543d1657cefd383b268405170
truong loc binh <binhtruong@beeketing.net>
2021-10-01 11:59:12 +0700

    Fix UI subscription specialist settings

--- src/specialist/settings/components/subscriptions/index.vue
+++ src/specialist/settings/components/subscriptions/index.vue
@@ -12,39 +12,20 @@
             .settings___card--internal.p-y-1
               .row
                 .col-md-12
-                  EmptyPlan
-            .settings___card--internal.p-y-1
-              .row
-                .col-md-12
                   h4.m-b-20 Plan
               .row
                 .col
                   Plan
-            // hr
-            // .settings___card--internal.p-y-1
-            //   .row
-            //     .col-md-6
-            //       h4
-            //         b Apps
-            //     .col-md-6.text-right
-            //       b-button.btn(type='button' variant='default') My Apps
-            //   .row
-            //     .col
-            //       AppsTable(:apps="apps")
-            //       div(v-if="!apps && apps.length") Apps not avaliable
 </template>
 
 <script>
   import Loading from '@/common/Loading/Loading'
-  // import AppsTable from "./components/AppsTable";
   import Plan from "./components/Plan";
   import EmptyPlan from "./components/EmptyPlan";
 
   export default {
     components: {
-      EmptyPlan,
       Plan,
-      // AppsTable,
       Loading,
     },
     data() {
